ec87fc32ada9b598103fdac11853d09b9d35fed3
Andrew G <andrew2000g@gmail.com>
2024-02-07 11:01:51 -0800

    files

--- fine_tune.py
+++ fine_tune.py
@@ -6,12 +6,19 @@ client = OpenAI()
 
 dataset = "addition"
 
-client.files.create(
+response = client.files.create(
   file=open(f"data/{dataset}_train.jsonl", "rb"),
   purpose="fine-tune"
 )
+print(response)
 client.files.create(
   file=open(f"data/{dataset}_validate.jsonl", "rb"),
   purpose="fine-tune"
 )
 
+client.fine_tuning.jobs.create(
+  training_file=f"{dataset}_train",
+  validation_file=f"{dataset}_validate",
+  suffix=f"{dataset}-finetuned",
+  model="davinci-002"
+)
