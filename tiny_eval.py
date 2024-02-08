f552817a6bfe769a7d17a977ced3d93db1911018
Andrew G <andrew2000g@gmail.com>
2024-02-07 20:09:06 -0800

    fix tiny eval

---
+++ tiny_eval.py
@@ -0,0 +1,24 @@
+from gpt_eval import eval_model_on_dataset
+from utils import get_model_name, get_joint_model_name, get_onlytiny_model_name
+
+
+def get_id_by_task_name(task_name):
+    if task_name in ["addy", "sub", "spain"]:
+        return 0
+    elif task_name in ["div", "mex", "mult2x1"]:
+        return 1
+    elif task_name in ["faith", "science"]:
+        return 2
+    else:
+        raise ValueError("Invalid task name")
+
+
+task_names = ["addy", "sub", "spain", "div", "mex", "mult2x1", "faith", "science"]
+
+for first_task_name in task_names:
+    model_finetuned_on_first_task_name = get_onlytiny_model_name(first_task_name, get_id_by_task_name(first_task_name))
+
+    correct, incorrect = eval_model_on_dataset(model_finetuned_on_first_task_name, first_task_name, get_id_by_task_name(first_task_name))
+
+    print(
+        f"Performance of {first_task_name}-tiny-finetuned model on {first_task_name} dataset: {correct / (correct + incorrect)}")
